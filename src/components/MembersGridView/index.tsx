9fe94bdeb9e8eae5492da73df186b08ee5a0186f
kennygunderman <kennygunderman@gmail.com>
2024-03-30 19:21:01 -0400

    Add bootstrap

--- src/components/MembersGridView/index.tsx
+++ src/components/MembersGridView/index.tsx
@@ -1,14 +1,16 @@
 //@ts-ignore
 import React from "react"
-import {kennyProfilePic} from "@assets/profile-pics";
 import './styles.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+import { kennyProfilePic } from "@assets/profile-pics";
 
 const GridView = () => {
     // Sample data
-    const people = [
-    ];
+    const people = [];
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 11; i++) {
         people.push(
             {
                 name: 'John Doe',
@@ -24,19 +26,17 @@ const GridView = () => {
         )
     }
 
-    // Managing this with raw CSS media queries sucks, we should probably just use bootstraps grid system:
-    // https://getbootstrap.com/docs/4.0/layout/grid/
-    return (
-        <div className="grid-container">
-            {people.map((person, index) => (
-                <div key={index} className="card">
-                    <div className="card-header">
-                        <img src={person.profilePicture} alt={person.name} className="profile-pic" />
-                        <div>
-                            <div className="card-name">{person.name}</div>
-                            <div className="card-tagline">{person.age}, Student, EST</div>
-                        </div>
+    const card = (person: any) => {
+        return (
+            <div key={0} className="card">
+                <div className="card-header">
+                    <img src={person.profilePicture} alt={person.name} className="profile-pic"/>
+                    <div>
+                        <div className="card-name">{person.name}</div>
+                        <div className="card-tagline">{person.age}, Student, EST</div>
                     </div>
+                </div>
+                <div className="card-body">
                     <p className="card-desc">{person.description}</p>
                     <div className="card-chips">
                         <div className="card-chip">Mobile Dev</div>
@@ -45,9 +45,22 @@ const GridView = () => {
                         <a href={person.links.instagram} className="card-chip">Skool Profile</a>
                     </div>
                 </div>
-            ))}
-        </div>
+            </div>
+        );
+    }
+
+    return (
+        <Container fluid={"xs"} className="grid-container">
+            <Row>
+                {people.map((person) =>
+                    <Col md={6} lg={4}>
+                        {card(person)}
+                    </Col>
+                )}
+            </Row>
+        </Container>
     );
 };
+
 
 export default GridView;
