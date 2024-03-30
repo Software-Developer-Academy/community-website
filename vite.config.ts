0331dafc87fbe9fd1ed6195c2af31b1220260223
Tim Ruscica <techwithtimm@gmail.com>
2024-03-30 17:28:52 -0300

    try to fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/community-website",
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
